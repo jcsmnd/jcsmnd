3594bcfbad3ba0146068d9fec11bd9d971822cfb
jcsmnd <jcsmnd@gmail.com>
2020-11-15 05:32:05 +0000

    .

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
-
 	$(window).scroll(function () {	
 		$(this).scrollTop() > 50 ? $('#back-to-top').fadeIn() : $('#back-to-top').fadeOut();
-		$(this).scrollTop() > 20 ? document.getElementById("navbar").style.padding = "0px 16px" : document.getElementById("navbar").style.padding = "16px 16px"
+		$(this).scrollTop() > 20 ? $("#navbar").css({"padding":"0px 16px", "opacity":"0.5"}).add(".navbar-brand").css({"font-size":"8px"}) : $("#navbar").css({"padding":"16px 16px", "opacity":"1.0"}).add(".navbar-brand").css({"font-size":"1.171875rem"});
 	});
 
 	$('#back-to-top').click(function () {
@@ -13,15 +12,14 @@ $(document).ready(function(){
 	});
 
 	TweenMax.to("#tiger", 2, {scale:0.8});
-
 	TweenMax.to("#tiger", 1, {boxShadow:"5px 5px 30px 10px rgba(255,255,255)",repeat:-1,yoyo:true});
-
+	
 	const tl = gsap.timeline({defaults: { ease: "power1.out" }});
-
 	tl.to(".hide", {y:"0%", duration: 2});
-
+	
 	$(function () {
 		$('[data-toggle="tooltip"]').tooltip()
 	});
-
+	
+	$('#musicPlayer').prop("volume", 0.3);
 });
