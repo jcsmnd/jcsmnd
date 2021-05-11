2fe36e35702f47633cd6780312d2db84cbbd28c3
jcsmnd <jcsmnd@gmail.com>
2021-05-11 05:20:57 +0000

    node.js + express in apache

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env nodejs
-var http = require('http');
-http.createServer(function (request, response) {
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.end('Node.js is working correctly!!\n');
-}).listen(8085);
-console.log('Server running at https://127.0.0.1:8085/');
+// var http = require('http');
+// http.createServer(function (request, response) {
+//    response.writeHead(200, {'Content-Type': 'text/plain'});
+//    response.end('Node.js is working correctly!!\n');
+// }).listen(8085);
+// console.log('Server running at https://127.0.0.1:8085/');
+
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => res.send('node.js + express in apache server using proxyPass'))
+app.listen(8085, () => console.log('Example app listening on port 8085!'))
